8d05666a8df01a93175f5ec734ef259ecbcfe237
sunmin <dlatjsals123@naver.com>
2022-03-04 12:17:05 +0900

    Component Event

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
     super(props);
     this.state ={
       mode:'read',
+      selected_content_id :1,
       subject:{title:'WEB', sub:'world wide web!'},
       welcome:{title:'Welcome', desc: 'Hello, React'},
       contents:[
@@ -26,8 +27,16 @@ class App extends Component {
       _title = this.state.welcome.title;
       _desc=this.state.welcome.desc;
     }else if(this.state.mode ==='read'){
-      _title=this.state.contents[0].title;
-      _desc=this.state.contents[0].desc;
+      let i =0;
+      while(i< this.state.contents.length){
+        let data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id){
+          _title=data.title;
+          _desc=data.desc;
+          break;
+        }
+        i=i+1;
+      }
     }
     return (
       <div className='App'>
@@ -37,15 +46,12 @@ class App extends Component {
             this.setState({mode:'welcome'});
           }.bind(this)}
           ></Subject>
-        {/* <header>
-          <h1><a href="/" onClick={function(e){
-            e.preventDefault(); //페이지가 새로고침되는것을 막아준다
-            //this.state.mode='welcome';
-            this.setState({mode: 'welcome'})
-          }.bind(this)}>{this.state.subject.title}</a></h1>
-          {this.state.subject.sub}
-        </header> */}
-        <TOC data={this.state.contents}></TOC>
+        <TOC onChangePage={function(id){
+          this.setState({
+            mode:'read',
+            selected_content_id:Number(id)
+          });
+        }.bind(this)}data={this.state.contents}></TOC>
         <Content title={_title} desc ={_desc}></Content>
       </div>
     );
